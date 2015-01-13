157f05e7fe9be043be1a132eed7f6830de1174a1
Eric DeLabar <delabar@careevolution.com>
2015-01-13 10:14:42 -0500

    Initial Commit

---
+++ Storyboards/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Storyboards
+//
+//  Created by Eric DeLabar on 1/13/15.
+//  Copyright (c) 2015 CareEvolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
